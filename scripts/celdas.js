8e2f22c6f4ef459303b3a17d184773530c1299f6
camilo-avendano1 <camilo.avendano1@udea.edu.co>
2021-09-17 22:51:10 -0500

    creacion casillas

---
+++ scripts/celdas.js
@@ -0,0 +1,15 @@
+class Cell(){
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+
+    }
+
+    //metodos
+    draw(){
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+    }
+}
---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = 900;
+canvas.height = 600;
+
+//variables globales
+const cellSize = 100;
+const cellGap = 3;
+const gameGrid = [];
+
+//gameboard
+const controlsBar = {
+    width: canvas.width,
+    height: cellSize,
+}
+function createGrid(){               //bucle para dibujar casillas
+    for (let y = cellSize; y < canvas.height; y += cellSize) {
+        for (let x = 0; x <canvas.width; x += cellSize){
+            gameGrid.push(new Cell(x,y));
+        }
+    }
+function handleGameGrid (){
+    for (let i= 0; i <gameGrid.length; i++){
+        gameGrid[i].draw();
+    }
+}
+}
+//proyectiles
+//defensa
+//enemigos
+//recursos
+//utilidades
+
+function animate(){
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0,0, controlsBar.width, controlsBar.height);
+    requestAnimationFrame(animate);
+}
+animate();
